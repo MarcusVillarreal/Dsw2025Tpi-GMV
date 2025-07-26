4bdb77d5e9cf76c2e575f3ad2b41345cca57f219
Marcos Villarreal <Gaston.Villarreal@alu.frt.utn.edu.ar>
2025-07-26 18:26:01 -0300

    ultima config en Dsw2025TpiContext

--- Dsw2025Tpi.Data/Dsw2025TpiContext.cs
+++ Dsw2025Tpi.Data/Dsw2025TpiContext.cs
@@ -8,4 +8,30 @@ public class Dsw2025TpiContext: DbContext
 	{
 
 	}
+
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+	/*
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.Entity<Category>(eb =>
+		{
+			eb.ToTable("Categories");
+			eb.Property(p => p.Name)
+			.HasMaxLength(50)
+			.IsRequired();
+		});
+		modelBuilder.Entity<Product>(eb =>
+		{
+			eb.ToTable("Products");
+			eb.Property(p => p.Sku)
+			.HasMaxLength(20)
+			.IsRequired();
+			eb.Property(p => p.Name)
+			.HasMaxLength(60);
+			eb.Property(p => p.CurrentUnitPrice)
+			.HasPrecision(15, 2);
+		});
+	*/
+	}
 }
