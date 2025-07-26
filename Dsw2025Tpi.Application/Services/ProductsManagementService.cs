3c8dc9a7738cfd0aca17d9ad81c918512af7f439
Marcos Villarreal <Gaston.Villarreal@alu.frt.utn.edu.ar>
2025-07-26 17:37:55 -0300

    congif Pro, PC, PMS, EfR, IR, Dsw2025TpiContext

---
+++ Dsw2025Tpi.Application/Services/ProductsManagementService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dsw2025Tpi.Domain.Interfaces;
+
+namespace Dsw2025Tpi.Application.Services;
+
+public class ProductsManagementService
+{
+	private readonly IRepository _repository;
+
+	public ProductsManagementService(IRepository repository)
+	{
+		_repository = repository;
+	}
+
+	//public async Task<ProductModel.Response?> GetProductById(Guid id)
+
+}
+
--- Dsw2025Tpi.Data/Dsw2025TpiContext.cs
+++ Dsw2025Tpi.Data/Dsw2025TpiContext.cs
@@ -4,4 +4,8 @@ namespace Dsw2025Tpi.Data;
 
 public class Dsw2025TpiContext: DbContext
 {
+	public Dsw2025TpiContext(DbContextOptions options) : base(options)
+	{
+
+	}
 }
