68bfb27915b0c6342feb81b2049913743690a4bb
Marcos Villarreal <Gaston.Villarreal@alu.frt.utn.edu.ar>
2025-07-31 18:00:27 -0300

    cambios en el controlador de servicio 2r1p

--- Dsw2025Tpi.Application/Services/ProductsManagementService.cs
+++ Dsw2025Tpi.Application/Services/ProductsManagementService.cs
@@ -33,8 +33,12 @@ public class ProductsManagementService
 			throw new ArgumentException("Valores para el producto no validos");
 		}
 		if (request.CurrentUnitPrice <= 0) throw new PriceNullException("El precio del producto no puede ser cero o menor."); var exist = await _repository.First<Product>(p => p.Sku == request.Sku);
+		
 		if (exist != null) throw new DuplicatedEntityException($"Ya existe un producto con el Sku {request.Sku}");
 		*/
+
+
+
 		var product = new Product(
 			request.Sku, 
 			request.InternalCode, 
@@ -42,7 +46,19 @@ public class ProductsManagementService
 			request.Description, 
 			request.CurrentUnitPrice, 
 			request.StockQuantity);
-		await _repository.Add(product);
+
+		//if (product.Sku is null)
+		//	throw new InvalidOperationException("El SKU no puede ser nulo.");
+		if (string.IsNullOrWhiteSpace(request.Sku) ||
+			string.IsNullOrWhiteSpace(request.InternalCode) ||
+			string.IsNullOrWhiteSpace(request.Description) ||
+			string.IsNullOrWhiteSpace(request.Name) ||
+			request.StockQuantity < 0)
+		{
+			throw new ArgumentException("Valores para el producto no validos");
+		}
+
+			await _repository.Add(product);
 		return new ProductModel.ProductResponse(product.Id, product.Sku, product.Name, product.CurrentUnitPrice, product.InternalCode, product.Description, product.StockQuantity);
 	}
 }
